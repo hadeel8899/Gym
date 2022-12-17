113926369a84e772dcec2c324a573ed7f330e64a
hadeel8899 <107553537+hadeel8899@users.noreply.github.com>
2022-12-17 21:17:40 +0200

    Add files via upload

--- gym/gym/doctype/store_rome/store_rome.js
+++ gym/gym/doctype/store_rome/store_rome.js
@@ -5,4 +5,17 @@ frappe.ui.form.on('Store rome', {
 	// refresh: function(frm) {
 
 	// }
+	// onload: function(frm) {
+	// 	show_alert('Hi, do you have a new message', 15);
+
+	// }
+	
+});
+frappe.call({
+    method: "gym.gym.doctype.store_rome.store_rome.check_if_doc_is_linked", //dotted path to server method
+    callback: function(r) {
+        // code snippet
+		show_alert('test', 15);
+
+    }
 });
--- gym/gym/doctype/store_rome/store_rome.py
+++ gym/gym/doctype/store_rome/store_rome.py
@@ -3,8 +3,24 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.model.rename_doc import get_link_fields
+
 
 class Storerome(Document):
 	pass
+
+
+@frappe.whitelist()
+def check_if_doc_is_linked():
+	# doc=frappe.get_all("store rome")
+	# link_fields = get_link_fields(doc.doctype)
+	# if link_fields :
+	# 	frappe.throw('hi hi hi ')
+	# else : 
+	# 	frappe.throw('Zerooo+9')
+
+	return
+
+    # // business logic 
